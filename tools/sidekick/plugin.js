8d902fb75e34efa15aa98325dc4f23513d13740d
mamadhavan <m.a.madhavan@gmail.com>
2026-04-08 22:54:38 +0200

    update

--- tools/sidekick/plugin.js
+++ tools/sidekick/plugin.js
@@ -7,22 +7,22 @@ export default function addScreenshotPlugin(sidekick) {
         const currentUrl = window.location.href;
 
         try {
-          const response = await fetch(
-            'https://42794-aemedsss-stage.adobeio-static.net/api/v1/web/aem-eds-ss/screenshot?url=' + encodeURIComponent(currentUrl)
-          );
+          const apiUrl = `https://runtime.adobe.io/api/v1/web/.../screenshot?url=${encodeURIComponent(
+            currentUrl,
+          )}`;
 
+          const response = await fetch(apiUrl);
           const base64 = await response.text();
 
-          const img = document.createElement('img');
-          img.src = 'data:image/png;base64,' + base64;
-
-          const win = window.open('');
-          win.document.write(img.outerHTML);
-
+          const link = document.createElement('a');
+          link.href = `data:image/png;base64,${base64}`;
+          link.download = 'screenshot.png';
+          link.click();
         } catch (e) {
-          alert('Screenshot failed: ' + e.message);
+          // eslint-disable-next-line no-alert
+          alert(`Screenshot failed: ${e.message}`);
         }
-      }
-    }
+      },
+    },
   });
 }
