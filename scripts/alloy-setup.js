8795be89ee45bc451e06f4de4285441023715fd1
mamadhavan <m.a.madhavan@gmail.com>
2026-08-07 11:25:49 +0200

    Update alloy-setup.js

--- scripts/alloy-setup.js
+++ scripts/alloy-setup.js
@@ -1,16 +1,16 @@
-export function loadAlloy() {
+export default function loadAlloy() {
   return new Promise((resolve) => {
     const check = setInterval(() => {
-      if(window.alloy && typeof window.alloy === 'function') {
+      if (window.alloy && typeof window.alloy === 'function') {
         clearInterval(check);
         resolve();
       }
-    },50);
+    }, 50);
 
     setTimeout(() => {
       clearInterval(check);
       console.error('[Target] window.alloy is not available after loadAlloy()');
       resolve();
-    },3000);
+    }, 3000);
   });
 }
