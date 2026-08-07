df90d8b1d51e03b2261d7232b84818eec0e73cda
mamadhavan <m.a.madhavan@gmail.com>
2026-08-07 12:00:25 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -96,8 +96,8 @@ async function configureAlloy() {
 
   // Step 4: Configure alloy with your Datastream ID and Org ID
   await window.alloy('configure', {
-    datastreamId: 'YOUR_ACTUAL_DATASTREAM_ID',
-    orgId: 'YOUR_ACTUAL_ORG_ID@AdobeOrg',
+    datastreamId: '3f75f0f0-4f07-482b-930a-8ef876cf2853',
+    orgId: 'E71EADC8584130D00A495EBD@AdobeOrg',
     defaultConsent: 'in',
     renderDecisions: false,
   });
